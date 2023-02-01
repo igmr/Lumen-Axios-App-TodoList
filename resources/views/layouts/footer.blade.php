b6c0a645615b6b9d98a7fc250de8021426617aae
= <=>
2023-02-01 17:28:18 -0600

    v.0.0.4 - Actualización de footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="footer mt-auto py-3 fixed-bottom bg-dark pt-9">
     <div class="container text-center">
-        <span class="text-muted">APP - Lista de tareas.</span>
+        <span class="text-muted">FontEnd | <strong>Lumen Framework</strong> - <strong>Bootstrasp 5</strong> - <strong>Axios</strong> </span><br>
+        <span class="text-muted">BackEnd | <strong>FligthPHP</strong> - <strong>MySQL</strong></span>
     </div>
 </footer>
